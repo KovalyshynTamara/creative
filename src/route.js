23dd5c5c73011e9e705dad0f10cae99c9787c77c
Tamara Kovalyshyn <kovtomara@gmail.com>
2023-03-22 16:56:05 +0200

    locatiom change

--- src/route.js
+++ src/route.js
@@ -3,15 +3,16 @@ import App from "./assets/App";
 import About from './pages/About';
 import Main from "./pages/Main";
 import Contacts1 from "./pages/Contacts1";
-import NotFound from "./pages/NotFound";
+// import NotFound from "./pages/NotFound";
 import Blog from "./pages/Blog";
-import { devHome, prodHome } from "./config";
+// import { devHome, prodHome } from "./config";
 
 
 const router = createHashRouter([
     
     {
-        path: window.location.hostname==='localhost' ? devHome : prodHome,
+        // path: window.location.hostname==='localhost' ? devHome : prodHome,
+        path: '/',
         element: <App/>,
         children: [
             {
